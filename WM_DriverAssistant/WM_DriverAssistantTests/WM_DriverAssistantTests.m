939dde09028931dac7cdb8a690feb387159dbf12
王雷 <wangleifool@163.com>
2016-10-27 17:23:50 +0800

    the initial of xcode project

---
+++ WM_DriverAssistant/WM_DriverAssistantTests/WM_DriverAssistantTests.m
@@ -0,0 +1,39 @@
+//
+//  WM_DriverAssistantTests.m
+//  WM_DriverAssistantTests
+//
+//  Created by 王雷 on 2016/10/27.
+//  Copyright © 2016年 王雷. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WM_DriverAssistantTests : XCTestCase
+
+@end
+
+@implementation WM_DriverAssistantTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
